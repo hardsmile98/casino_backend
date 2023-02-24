60a4cec92cae74f498223f97e409c55d8757fffd
Денис Котелев <deniskotelev@MacBook-Pro-Denis.local>
2023-02-24 23:14:12 +0300

    feat: port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const { createServer } = require('http');
 const cors = require('cors')
 const app = express();
-const port = process.env.PORT || 3030;
+const port = 3030;
 const wssPort = process.env.PORT || 5005;
 const server = createServer(app).listen(wssPort);
 
