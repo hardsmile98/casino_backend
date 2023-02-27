17ac2187bc08414d9dd074d57591ee9da75515ec
Денис Котелев <deniskotelev@MacBook-Pro-Denis.local>
2023-02-27 20:12:00 +0300

    feat: tables

--- app.js
+++ app.js
@@ -1,78 +1,93 @@
-require('dotenv').config();
-const WebSocket = require('ws');
-const express = require('express');
-const { createServer } = require('http');
+require('dotenv').config()
+const WebSocket = require('ws')
+const express = require('express')
+const { createServer } = require('http')
 const cors = require('cors')
-const app = express();
-const port = 3030;
-const wssPort = 8030;
-const server = createServer(app).listen(wssPort);
-const { connectToTable } = require('./connectToTable');
+const app = express()
+const port = 3030
+const wssPort = 8030
+const server = createServer(app).listen(wssPort)
+const { connectToTable } = require('./connectToTable')
 
 // * Переменные казино  *//
 const tables = [
   {
-    casinoUrl: 'gs6.pragmaticplaylive.net',
+    casinoUrl: 'gs5.pragmaticplaylive.net',
     tableId: 'g03y1t9vvuhrfytl'
-  },
-];
+  }
+]
 
-function appStart() {
+function appStart () {
   // * Global values * //
-  let resultGames = {};
-  let isConnection = true;
-  let sessionId = '';
+  const resultGames = {}
+  const isConnection = true
+  let wsTables = []
+  let sessionId = ''
 
-  const wssServer = new WebSocket.Server({ server });
+  const wssServer = new WebSocket.Server({ server })
 
-  wssServer.on("connection", ws => {
-    if(!isConnection) {
+  wssServer.on('connection', ws => {
+    if (!isConnection) {
       ws.send(JSON.stringify({ event: 'noConnection' }))
-      return;
+      return
     }
 
-    ws.send(JSON.stringify({ 
-      event: 'games', 
-      data: resultGames,
-    }));
+    ws.send(JSON.stringify({
+      event: 'init',
+      data: resultGames
+    }))
   })
 
   const connectToAllTables = () => {
     tables.forEach((table) => {
-      connectToTable({
+      const wsTable = connectToTable({
         table,
         sessionId,
         resultGames,
         isConnection,
-        wssServer,
+        wssServer
       })
+
+      wsTables.push(wsTable)
     })
   }
 
-  connectToAllTables();
+  const closeAllTables = () => {
+    wsTables.forEach((table) => {
+      if (table.OPEN) {
+        table.close()
+      }
+    })
+
+    wsTables = []
+  }
+
+  connectToAllTables()
 
   app.use(cors({
     credentials: true,
-    origin: process.env.CLIENT_URL,
-  }));
-  app.use(express.json());
+    origin: process.env.CLIENT_URL
+  }))
+  app.use(express.json())
 
   app.post('/api/newSessionId', function (req, res) {
-    const { body } = req;
-    const { sessionId: newId } = body;
+    const { body } = req
+    const { sessionId: newId } = body
 
     console.log('newSessionId', newId)
 
-    sessionId = newId;
+    closeAllTables()
 
-    connectToAllTables();
+    sessionId = newId
 
-    res.status(200).send({ success: true });
-  });
+    connectToAllTables()
+
+    res.status(200).send({ success: true })
+  })
 
   app.listen(port, () => {
     console.log(`server app on port ${port}`)
   })
 };
 
-appStart();
+appStart()
--- connectToTable.js
+++ connectToTable.js
@@ -1,114 +1,105 @@
 const request = require('request')
-const WebSocket = require('ws');
+const WebSocket = require('ws')
 
-const gamesCount = 20;
+const gamesCount = 20
 
-let reconectCount = 0;
-  
+let reconectCount = 0
+
 // Получение данных с из стола
-function connectToTable({ 
-    table,
-    sessionId,
-    resultGames,
-    isConnection,
-    wssServer,
+function connectToTable ({
+  table,
+  sessionId,
+  resultGames,
+  isConnection,
+  wssServer
 }) {
-    const { casinoUrl, tableId } = table || {};
+  const { casinoUrl, tableId } = table || {}
 
-    const wsUrl = `wss://${casinoUrl}/game?JSESSIONID=${sessionId}&tableId=${tableId}`;
-    const getHistoryUrl = `https://${casinoUrl}/api/ui/statisticHistory?tableId=${tableId}&numberOfGames=${gamesCount}&JSESSIONID=${sessionId}`
+  const wsUrl = `wss://${casinoUrl}/game?JSESSIONID=${sessionId}&tableId=${tableId}`
+  const getHistoryUrl = `https://${casinoUrl}/api/ui/statisticHistory?tableId=${tableId}&numberOfGames=${gamesCount}&JSESSIONID=${sessionId}`
 
-    const ws = new WebSocket(wsUrl, {});
+  const ws = new WebSocket(wsUrl, {})
 
-    const sendGames = () => {
-      console.log('send games');
-    
-      try {
-        request(
-          getHistoryUrl,
-          (err, response, body) => {
-            const json = JSON.parse(body);
-            const results = json?.history?.map(game => game?.gameResult);
+  const sendGames = () => {
+    console.log('send games')
 
-            resultGames[tableId] = results;
+    try {
+      request(
+        getHistoryUrl,
+        (_err, _response, body) => {
+          const json = JSON.parse(body)
+          const results = json?.history?.map(game => game?.gameResult)
 
-            // Отсылаем результаты игр на клиент через сокет
-            wssServer.clients.forEach(function each(client) {
-              if (client.readyState === WebSocket.OPEN) {
-                client.send(JSON.stringify({
-                  event: 'games',
-                  tableId,
-                  data: results,
-                }));
-              }
-            });
-          }
-        )
-      } catch (error) {
-        console.log(error);
-      }
+          resultGames[tableId] = results
 
-    }
-
-    ws.onclose = () => {
-      if (reconectCount === 0) {
-        
-        reconectCount++;
-        connectToTable({ 
-            table,
-            sessionId,
-            resultGames,
-            isConnection,
-            wssServer,
-        })
-        return;
-      }
-
-
-      isConnection = false;
-
-      wssServer.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(JSON.stringify({
-            event: 'noConnection',
-          }));
+          // Отсылаем результаты игр на клиент через сокет
+          wssServer.clients.forEach(function each (client) {
+            if (client.readyState === WebSocket.OPEN) {
+              client.send(JSON.stringify({
+                event: 'games',
+                tableId,
+                data: results
+              }))
+            }
+          })
         }
-      });
+      )
+    } catch (error) {
+      console.log(error)
     }
-
-    ws.onopen = () => {
-      isConnection = true;
-    
-      wssServer.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(JSON.stringify({
-            event: 'connection',
-          }));
-        }
-      });
-
-      sendGames();
-    }
-
-    ws.onerror = () => {
-      isConnection = false;
-    }
-
-    ws.on('message', (data) => {
-      const chank = data.toString().replace(/^\d+/, '');
-      if (chank !== '') {
-        // выпадение числа
-        const isEndGame =  chank.includes('zoomOut');
-
-        if(isEndGame) {
-          if(reconectCount !== 0) {
-            reconectCount = 0;
-          }
-
-          sendGames();
-        }
-      }
-    });
   }
 
-  module.exports = { connectToTable }
+  ws.onclose = () => {
+    if (reconectCount === 0) {
+      reconectCount++
+
+      connectToTable({
+        table,
+        sessionId,
+        resultGames,
+        isConnection,
+        wssServer
+      })
+
+      return
+    }
+
+    isConnection = false
+
+    wssServer.clients.forEach(function each (client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({
+          event: 'noConnection'
+        }))
+      }
+    })
+  }
+
+  ws.onopen = () => {
+    isConnection = true
+  }
+
+  ws.onerror = () => {
+    isConnection = false
+  }
+
+  ws.on('message', (data) => {
+    const chank = data.toString().replace(/^\d+/, '')
+    if (chank !== '') {
+      // выпадение числа
+      const isEndGame = chank.includes('zoomOut')
+
+      if (isEndGame) {
+        if (reconectCount !== 0) {
+          reconectCount = 0
+        }
+
+        sendGames()
+      }
+    }
+  })
+
+  return ws
+}
+
+module.exports = { connectToTable }
